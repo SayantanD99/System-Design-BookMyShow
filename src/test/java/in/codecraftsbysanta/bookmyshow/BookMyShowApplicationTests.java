d8ee45f9496da7ff48a42c1c684f96978b3cd830
Sayantan Das <32683338+SayantanD99@users.noreply.github.com>
2024-09-19 08:43:23 +0530

    Add files via upload

--- src/test/java/in/codecraftsbysanta/bookmyshow/BookMyShowApplicationTests.java
+++ src/test/java/in/codecraftsbysanta/bookmyshow/BookMyShowApplicationTests.java
@@ -1,13 +1,46 @@
 package in.codecraftsbysanta.bookmyshow;
 
+import in.codecraftsbysanta.bookmyshow.controllers.UserController;
+import in.codecraftsbysanta.bookmyshow.dtos.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookMyShowApplicationTests {
+class BookMyShowMay24ApplicationTests {
+    @Autowired
+    private UserController userController;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void testSignUpFunctionality() {
+        SignupRequestDto requestDto = new SignupRequestDto();
+        requestDto.setName("Jegan");
+        requestDto.setEmail("jegan@scaler.com");
+        requestDto.setPassword("123456");
+
+        SignupResponseDto responseDto = userController.signUp(
+                requestDto
+        );
+
+        System.out.println(responseDto.getUserId());
+    }
+
+    @Test
+    public void testLoginFunctionality() {
+        LoginRequestDto requestDto = new LoginRequestDto();
+        requestDto.setEmail("zahidul@scaler.com");
+        requestDto.setPassword("abcD");
+
+        LoginResponseDto responseDto = userController.login(requestDto);
+
+        if (responseDto.getResponseStatus().equals(ResponseStatus.SUCCESS)) {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Login Unsuccessful");
+        }
+    }
 }
